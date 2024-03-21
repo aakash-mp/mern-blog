fa05f90d55dc970d63721fc11aee7299b6b5d4cc
aakash-mp <aakash.a.ac@gmail.com>
2024-03-21 19:04:01 +0530

    DB connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express  from "express";
+import express  from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURL)
+.then(()=>{
+
+    console.log("DB connected successfully");
+}).catch((err)=> {
+
+    console.log(err);
+});
 
 const app = express();
-
 app.listen(3000, () =>{
     console.log("listening to port 3000");
 });
