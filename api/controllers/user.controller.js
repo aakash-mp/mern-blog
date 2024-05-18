9eb283df78a85e6b394951bd68ab3f8a9cc25e72
aakash-mp <aakash.a.ac@gmail.com>
2024-05-18 21:10:26 +0530

    complete update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -30,6 +30,7 @@ export const updateUser = async(req, res, next) => {
         if(!req.body.username.match(/^[a-zA-Z0-9]+$/)){
             return next(errorHandler(400, "username can only contains letters and numbers"));
         }
+    }
         try {
             const updatedUser = await User.findByIdAndUpdate(
                 req.params.userId,
@@ -48,8 +49,5 @@ export const updateUser = async(req, res, next) => {
             
              } catch (error) {
                  next(error);
-            
             }
-    }
-
-}
+};
