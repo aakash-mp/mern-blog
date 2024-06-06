f33a5bf3cef3b80fa18b23762ac85cada91740d6
aakash-mp <aakash.a.ac@gmail.com>
2024-06-06 17:01:01 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
-import express  from "express"
-import mongoose from "mongoose"
+import express  from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
-import authRoutes from "./routes/auth.route.js"
-import postRoutes from "./routes/post.route.js"
-import commentRoutes from "./routes/comment.route.js"
-import cookieParser from 'cookie-parser'
-
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -18,6 +18,8 @@ mongoose.connect(process.env.MONGOURL)
 
     console.log(err);
 });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -33,6 +35,11 @@ app.use('/api/auth',authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 
 app.use((err, req, res, next) =>{
